f777d8e507725d2669fac89723829b7a736a5991
jnosis <jnosis1546@gmail.com>
2022-07-07 16:18:50 +0900

    Add checking auth when add, update and delete Work

--- src/router/work.ts
+++ src/router/work.ts
@@ -1,14 +1,15 @@
 import { Router } from 'opine';
 import { IWorkController } from './../types.ts';
+import { isAuth } from './../middleware/auth.ts';
 
 const router = Router();
 
 export default function workRouter(workController: IWorkController) {
   router.get('/', workController.getAll);
   router.get('/:id', workController.getByTitle);
-  router.post('/', workController.add);
-  router.put('/:id', workController.update);
-  router.delete('/:id', workController.delete);
+  router.post('/', isAuth, workController.add);
+  router.put('/:id', isAuth, workController.update);
+  router.delete('/:id', isAuth, workController.delete);
 
   return router;
 }
