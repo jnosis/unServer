4b3640d3143566fa28bc0ecc08d13899476e3346
jnosis <jnosis1546@gmail.com>
2023-11-18 01:29:43 +0900

    feat: Add validator for hono

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,4 +1,5 @@
-import { NextFunction, OpineRequest, OpineResponse } from 'opine';
+import type { NextFunction, OpineRequest, OpineResponse } from 'opine';
+import { createMiddleware } from 'hono/helper';
 import { z, ZodRawShape } from 'zod';
 import { throwError } from '~/middleware/error_handler.ts';
 
@@ -23,3 +24,23 @@ export const validate = (schema: ZodRawShape) => [async (
     message: result.error.errors[0].message,
   });
 }];
+
+export const hValidate = (schema: ZodRawShape) => {
+  return createMiddleware(async (c, next) => {
+    const validation = z.object(schema);
+    const result = await validation.safeParseAsync(await c.req.json());
+
+    if (!result.success) {
+      const { method, path } = c.req;
+
+      return throwError({
+        method,
+        baseUrl: path,
+        status: 400,
+        message: result.error.errors[0].message,
+      });
+    }
+
+    await next();
+  });
+};
