1afde6f05af90885d1865679778a49b0cd1ae867
jnosis <jnosis1546@gmail.com>
2023-11-18 01:37:47 +0900

    feat: Add Work validation for hono

--- src/router/work.ts
+++ src/router/work.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 import { Router } from 'opine';
 import { z } from 'zod';
 import { isAuth, isHAuth } from '~/middleware/auth.ts';
-import { validate } from '~/middleware/validator.ts';
+import { hValidate, validate } from '~/middleware/validator.ts';
 
 const router = Router();
 
@@ -35,11 +35,27 @@ export default function workRouter(workController: IWorkController) {
 
 const work = new Hono();
 
+const hValidateWork = hValidate({
+  title: z.string().min(1, { message: 'Title should be not empty' }),
+  description: z.string(),
+  repo: z.object({
+    url: z.string().regex(
+      /^(https?\:\/\/)?github.com\/[\w]+\/[\w]+/,
+      'Invalid repository url',
+    ),
+    branch: z.string(),
+  }),
+  projectUrl: z.union([
+    z.literal(''),
+    z.string().url({ message: 'Invalid project url' }),
+  ]),
+});
+
 export function hWorkRouter(workController: IHWorkController) {
   work.get('/', workController.getAll);
   work.get('/:id', workController.getByTitle);
-  work.post('/', isHAuth, workController.add);
-  work.put('/:id', isHAuth, workController.update);
+  work.post('/', isHAuth, hValidateWork, workController.add);
+  work.put('/:id', isHAuth, hValidateWork, workController.update);
   work.delete('/:id', isHAuth, workController.delete);
 
   return work;
