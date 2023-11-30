db3e58f6f2d8a7432a80d207097cc359505248ad
jnosis <jnosis1546@gmail.com>
2023-11-30 13:10:51 +0900

    test: Replace faker.datatype.json to createData

--- src/tests/api_utils.ts
+++ src/tests/api_utils.ts
@@ -4,11 +4,25 @@ import { Hono } from 'hono';
 export function createRouter() {
   const root = faker.system.directoryPath();
   const path = '/' + faker.word.sample();
-  const data = JSON.parse(faker.datatype.json());
+  const data = createData();
 
   const router = new Hono().get(path, (c) => {
     return c.json(data);
   });
 
   return { root, router, path, data };
+}
+
+function createData() {
+  const properties = ['foo', 'bar', 'bike', 'a', 'b', 'name', 'prop'];
+  const data: Record<string, string | number> = {};
+
+  for (const property of properties) {
+    if (faker.datatype.boolean()) continue;
+    data[property] = faker.datatype.boolean()
+      ? faker.string.sample()
+      : faker.number.int();
+  }
+
+  return data;
 }
