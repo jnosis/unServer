d5e0db4b5c7e282ef5d13e205267380b7a6a602f
jnosis <jnosis1546@gmail.com>
2024-01-21 04:40:42 +0900

    fix: Modify to remove middlewares from /api res

--- src/util/endpoints.ts
+++ src/util/endpoints.ts
@@ -1,9 +1,9 @@
 import type { Hono } from 'hono';
 
 export function getEndPoints(root: string, router: Hono) {
-  const endPoints = router.routes.map(({ path, method }) =>
-    `${method} ${root}${path}`
-  ).flat();
+  const endPoints = router.routes
+    .filter(({ handler }) => handler.length <= 1)
+    .map(({ path, method }) => `${method} ${root}${path}`).flat();
 
   return endPoints;
 }
