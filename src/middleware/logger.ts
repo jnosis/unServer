719ecbcaef4dcf41e78f0aea152ba88755eeebc4
jnosis <jnosis1546@gmail.com>
2024-02-17 02:24:20 +0900

    feat: Add color to status code in logs

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,4 +1,5 @@
 import { createMiddleware } from 'hono/helper';
+import { cyan, green, magenta, red, yellow } from '$std/fmt/colors.ts';
 import { ConsoleHandler, getLogger, setup } from '$std/log/mod.ts';
 import { convertToMessage } from '~/util/message.ts';
 
@@ -12,7 +13,11 @@ setup({
           timeZoneName: 'short',
         });
         const args = logRecord.args.length > 0
-          ? `${logRecord.args.join(' ')} `
+          ? `${
+            logRecord.args.map((arg, i) =>
+              i === 2 ? colorStatus(arg as number) : arg
+            ).join(' ')
+          } `
           : '';
         return `${time} [${logRecord.levelName}] ${args}${logRecord.msg}`;
       },
@@ -50,3 +55,19 @@ export const logger = createMiddleware(async (c, next) => {
   if (status < 400) log.debug(msg, ...args);
   else log.error(msg, ...args);
 });
+
+const colorStatus = (status: number) => {
+  const out: { [key: string]: string } = {
+    7: magenta(`${status}`),
+    5: red(`${status}`),
+    4: yellow(`${status}`),
+    3: cyan(`${status}`),
+    2: green(`${status}`),
+    1: green(`${status}`),
+    0: yellow(`${status}`),
+  };
+
+  const calculateStatus = (status / 100) | 0;
+
+  return out[calculateStatus];
+};
