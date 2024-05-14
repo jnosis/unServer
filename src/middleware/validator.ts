042b61091d9d1b283a3fb7e96e9115c764f226aa
jnosis <jnosis1546@gmail.com>
2024-05-14 12:33:24 +0900

    feat: Add content-type checking to validator

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -5,7 +5,13 @@ import { throwError } from '~/middleware/error_handler.ts';
 export const validate = (schema: ZodRawShape) => {
   return createMiddleware(async (c, next) => {
     const validation = z.object(schema);
-    const result = await validation.safeParseAsync(await c.req.json());
+    const contentType = c.req.header('Content-Type');
+
+    const result = await validation.safeParseAsync(
+      isFormData(contentType || '')
+        ? await c.req.parseBody()
+        : await c.req.json(),
+    );
 
     if (!result.success) {
       return throwError(400, result.error.errors[0].message);
@@ -14,3 +20,7 @@ export const validate = (schema: ZodRawShape) => {
     await next();
   });
 };
+
+function isFormData(contentType: string): boolean {
+  return contentType.includes('multipart/form-data');
+}
