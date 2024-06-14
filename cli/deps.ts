104d26e7c84782f4601204bd6e5e866e1b19d8d7
jnosis <jnosis1546@gmail.com>
2024-06-14 14:33:22 +0900

    refactor(cli): Change cliffy import to jsr

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,7 +1,2 @@
-export { Command } from 'https://deno.land/x/cliffy@v0.25.6/command/mod.ts';
-export {
-  Checkbox,
-  Confirm,
-  Input,
-  List,
-} from 'https://deno.land/x/cliffy@v0.25.6/prompt/mod.ts';
+export { Command } from '@cliffy/command';
+export { Checkbox, Confirm, Input, List } from '@cliffy/prompt';
