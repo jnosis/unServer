f3de11f09bd0e0b98e27c9a17ea7b59c71922028
jnosis <jnosis1546@gmail.com>
2024-07-03 16:10:49 +0900

    feat: Add alert when an error occurs using Join

--- src/middleware/error_handler.ts
+++ src/middleware/error_handler.ts
@@ -1,20 +1,37 @@
-import type { ErrorHandler, NotFoundHandler } from 'hono';
+import type { Context, ErrorHandler, NotFoundHandler } from 'hono';
 import type { StatusCode } from 'hono/utils/http-status';
 import { HTTPException } from 'hono/http-exception';
+import { JoinJoaomgcd } from 'joinjoaomgcd';
+import config from '~/config.ts';
+
+const joinjoaomgcd = new JoinJoaomgcd(config.join.apiKey);
 
 export const throwError = (status: StatusCode, message: string) => {
   throw new HTTPException(status, { message });
 };
 
-export const errorHandler: ErrorHandler = (err, c) => {
+export const errorHandler: ErrorHandler = async (err, c) => {
   const message = err.message || 'Something is wrong';
   if (err instanceof HTTPException) {
     const res = err.getResponse();
+    await sendErrorToDevice(c, message, res.status);
     return c.json({ message }, res);
   }
+  await sendErrorToDevice(c, 'Internal Server Error', 500);
   return c.text('Internal Server Error', 500);
 };
 
-export const notFoundHandler: NotFoundHandler = (c) => {
-  return c.json({ message: `Route(${c.req.path}) not found` }, 404);
+export const notFoundHandler: NotFoundHandler = async (c) => {
+  const message = `Route(${c.req.path}) not found`;
+  await sendErrorToDevice(c, message, 404);
+  return c.json({ message }, 404);
 };
+
+async function sendErrorToDevice(c: Context, message: string, status: number) {
+  const { method, path } = c.req;
+  await joinjoaomgcd.sendPush({
+    deviceId: config.join.deviceId,
+    title: `unServer Error on ${method} ${path} ${status}`,
+    text: message,
+  });
+}
